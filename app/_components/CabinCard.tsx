a97547e758f31ca64a2bba6cd9cad1b6bebbcc70
Rachelle Zhu <darkd7131@gmail.com>
2024-12-23 20:51:59 +0700

    add some starter files

---
+++ app/_components/CabinCard.tsx
@@ -0,0 +1,52 @@
+import { UsersIcon } from "@heroicons/react/24/solid";
+import { TCabin } from "../_types/cabin-type";
+
+export default function CabinCard({ cabin }: { cabin: TCabin }) {
+  const { id, name, max_capacity, regular_price, discount, image_url } = cabin;
+
+  return (
+    <div className="flex border-primary-800 border">
+      <img src={image_url!} alt={`Cabin ${name}`} />
+
+      <div className="flex-grow">
+        <div className="pt-5 pb-4 px7 bg-primary-950">
+          <h3 className="text-accent-500 font-semibold text-2xl mb-3">
+            Cabin{name}
+          </h3>
+
+          <div className="flex gap-3 items-center mb-2">
+            <UsersIcon className="h-5 w-5 text-primary-600" />
+            <p className="text-lg text-primary-200">
+              For up to <span className="font-bold">{max_capacity}</span>
+            </p>
+          </div>
+
+          <p className="flex gap-3 justify-end items-baseline">
+            {discount !== null && discount > 0 ? (
+              <>
+                <span className="text-3xl font-[350]">
+                  ${regular_price! - discount!}
+                </span>
+                <span className="line-through font-semibold text-primary-600">
+                  ${regular_price!}
+                </span>
+              </>
+            ) : (
+              <span className="text-3xl font-[350]">${regular_price!}</span>
+            )}{" "}
+            <span className="text-primary-200">/ night</span>
+          </p>
+        </div>
+
+        <div className="bg-primary-950 border-t border-t-primary-800 text-right">
+          <a
+            href={`/cabins/${id}`}
+            className="border-l border-primary-800 py-4 px-6 inline-block hover:bg-accent-600 transition-all hover:text-primary-900"
+          >
+            Details & reservation &rarr;
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,20 @@
 export default function Page() {
   return (
-    <div>
-      <h1>The Wild Oasis. Welcome to paradise.</h1>
-    </div>
+    <main>
+      <img src="bg.png" alt="Mountains and forests with two cabins" />
+
+      <div className="relative z-10 text-center">
+        <h1 className="text-8xl text-primary-50 mb-10 tracking-tight font-normal">
+          Welcome to paradise
+        </h1>
+
+        <a
+          href="/cabins"
+          className="bg-accent-500 px-8 py-6 text-primary-800 text-lg font-semibold hover:bg-accent-600 transition-all"
+        >
+          Explore luxury cabins
+        </a>
+      </div>
+    </main>
   );
 }
