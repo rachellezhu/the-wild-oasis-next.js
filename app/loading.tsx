c5162f1e9be7ca8a0bf661e7cd472d505674efea
Rachalle Zhu <darkd7131@gmail.com>
2024-12-25 02:10:06 +0700

    change spinner

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,9 +1,9 @@
-import SpinnerMini from "@/app/_components/SpinnerMini";
+import Spinner from "@/app/_components/Spinner";
 
 export default function Loading() {
   return (
     <div>
-      <SpinnerMini />
+      <Spinner />
     </div>
   );
 }
