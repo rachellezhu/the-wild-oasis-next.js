0910c445ae3f5ddd2fb9e33577f0136db281de22
Rachalle Zhu <darkd7131@gmail.com>
2025-01-09 22:34:18 +0700

    fix typo

--- app/thank-you/page.tsx
+++ app/thank-you/page.tsx
@@ -1,6 +1,6 @@
 export default function Page() {
   return (
-    <div className="text-center space-y-6 mt4">
+    <div className="text-center space-y-6 mt-4">
       <h1 className="text-3xl font-semibold">
         Thank you for your reservation!
       </h1>
